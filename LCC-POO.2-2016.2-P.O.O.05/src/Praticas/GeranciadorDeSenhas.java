a4bdce73b2af03c1c4dcbc2785eb1675ead9d509
MarceloSSantos <marcelo.souza@dce.ufpb.br>
2017-04-21 14:02:55 -0300

    Atualização das atividades.

---
+++ LCC-POO.2-2016.2-P.O.O.05/src/Praticas/GeranciadorDeSenhas.java
@@ -0,0 +1,26 @@
+package Praticas;
+
+public class GeranciadorDeSenhas {
+	private boolean prioeidadePorIdade;
+
+	public GeranciadorDeSenhas(boolean prioeidadePorIdade) {
+		this.prioeidadePorIdade = prioeidadePorIdade;
+	}
+
+	public int emitirSenha(String nome, int idade, int cpf) {
+		return 0;
+	}
+
+	public int ProximaSenha() {
+		return 0;
+	}
+
+	public boolean isPrioeidadePorIdade() {
+		return prioeidadePorIdade;
+	}
+
+	public void setPrioeidadePorIdade(boolean prioeidadePorIdade) {
+		this.prioeidadePorIdade = prioeidadePorIdade;
+	}
+
+}
---
+++ LCC-POO.2-2016.2-P.O.O.05/src/Praticas/Senha.java
@@ -0,0 +1,44 @@
+package Praticas;
+
+public class Senha {
+	private String nome;
+	private int idade;
+	private int cpf;
+
+	public Senha() {
+		this.nome = "";
+		this.idade = 0;
+		this.cpf = 0;
+	}
+
+	public Senha(String nome, int idade, int cpf) {
+		this.nome = nome;
+		this.idade = idade;
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+}
