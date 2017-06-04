575b8b9adf3115b1cf4489b96e6a61c37460f829
MarceloSSantos <marcelo.souza@dce.ufpb.br>
2017-06-04 17:45:32 -0300

    Thread

--- LCC-POO.2-2016-P.O.O.07/src/Questao01/Questao01.java
+++ LCC-POO.2-2016-P.O.O.07/src/Questao01/Questao01.java
@@ -5,10 +5,24 @@ public class Questao01 {
 		Questao001 q1 = new Questao001(1, 10);
 		Questao001 q2 = new Questao001(11, 20);
 		Questao001 q3 = new Questao001(21, 30);
+		Questao001 q4 = new Questao001(31, 40);
+		Questao001 q5 = new Questao001(41, 50);
+		Questao001 q6 = new Questao001(51, 60);
+		Questao001 q7 = new Questao001(61, 70);
+		Questao001 q8 = new Questao001(71, 80);
+		Questao001 q9 = new Questao001(81, 90);
+		Questao001 q10 = new Questao001(91, 99);
 
 		q1.start();
 		q2.start();
 		q3.start();
+		q4.start();
+		q5.start();
+		q6.start();
+		q7.start();
+		q8.start();
+		q9.start();
+		q10.start();
 	}
 
 }
